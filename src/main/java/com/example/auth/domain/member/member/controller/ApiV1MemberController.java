a58eb64a595d4214b90c51625d7b96dbb784be5e
linedj <eunjun998@gmail.com>
2025-02-03 15:18:55 +0900

    login

--- src/main/java/com/example/auth/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/auth/domain/member/member/controller/ApiV1MemberController.java
@@ -45,4 +45,26 @@ public class ApiV1MemberController {
         );
     }
 
+
+    record LoginReqBody(@NotBlank @Length(min = 3) String username,
+                        @NotBlank @Length(min = 3) String password) {
+    }
+
+    @PostMapping("/login")
+    public RsData<String> login(@RequestBody @Valid LoginReqBody body) {
+
+        Member actor = memberService.findByUsername(body.username())
+                .orElseThrow(() -> new ServiceException("401-2", "아이디 또는 비밀번호가 일치하지 않습니다."));
+
+        if(!actor.getPassword().equals(body.password())) {
+            throw new ServiceException("401-2", "아이디 또는 비밀번호가 일치하지 않습니다.");
+        }
+
+        return new RsData<>(
+                "200-1",
+                "%s님 환영합니다.".formatted(actor.getNickname()),
+                actor.getApiKey()
+        );
+    }
+
 }
